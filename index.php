1d010b8dbfcf403ec8908e051c27a795e2a5b64b
Scott Whittell <scott.whittell@btinternet.com>
2020-03-18 16:33:45 +0000

    header/footer + form

---
+++ index.php
@@ -0,0 +1,216 @@
+
+<?php echo file_get_contents("html/header.html"); ?>           
+
+                <main id="content">
+                    <div class="slides">
+                        <div class="banner bannerweb">
+                            <div class="bcontainer">
+                                <h2>WEB DESIGN</h2>
+                                <p>For businesses looking to make a strong <br>and effective first impression.</p>
+                                <a href="https://support.netmatters.com/web-design" class="bannerb web-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="banner bannerit">
+                            <div class="bcontainer">
+                                <h2>IT SUPPORT</h2>
+                                <p>Fast and cost-effective IT support <br>services for your business.</p>
+                                <a href="https://support.netmatters.com/web-design" class="bannerb it-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="banner bannertele">
+                            <div class="bcontainer">
+                                <h2>TELECOMS SERVICES</h2>
+                                <p>A new approach to connectivity, see how <br>we can help your business.</p>
+                                <a href="https://support.netmatters.com/telecoms-services" class="bannerb tele-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="banner bannerbesp">
+                            <div class="bcontainer">
+                                <h2>BESPOKE SOFTWARE</h2>
+                                <p>Bring your business together with solutions that grow with you.</p>
+                                <a href="https://support.netmatters.com/bespoke-software" class="bannerb besp-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="banner bannerdigi">
+                            <div class="bcontainer">
+                                <h2>DIGITAL MARKETING</h2>
+                                <p>Generating you new business through results-driven marketing activities.</p>
+                                <a href="https://support.netmatters.com/digital-marketing" class="bannerb digi-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="banner bannercyber">
+                            <div class="bcontainer">
+                                <h2>CYBER SECURITY</h2>
+                                <p>Generating you new business through results-driven marketing activities.</p>
+                                <a href="https://support.netmatters.com/cyber-security" class="bannerb cyber-b">
+                                    FIND OUT MORE<i class="fa fa-arrow-right arrowb"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <section class="cardset">
+                        <div id="cardset1">
+                            <a href="https://support.netmatters.com/web-design">
+                                <div class="nmcard" id="webcard">
+                                    <div class="testnm icircle" id="codeb"><i class="fas fa-code"></i></div>
+                                    <h5>Web Design</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">User-centric design for businesses looking to make a lasting first
+                                        impression
+                                        and drive conversions.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/it-support">
+                                <div class="nmcard" id="itcard">
+                                    <div class="icircle" id="deskb"><i class="fas fa-desktop"></i></div>
+                                    <h5>IT Support</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Remotely managed IT services that is catered to your business needs,
+                                        adds
+                                        value
+                                        and reduces costs.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div id="cardset2">
+
+                            <a href="https://support.netmatters.com/telecoms-services">
+                                <div class="nmcard" id="telcard">
+                                    <div class="icircle phoneb"><i class="fas fa-phone-volume"></i></div>
+                                    <h5>Telecoms Services</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Stay connected with bespoke telecoms solutions when you need it
+                                        most.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/bespoke-software">
+                                <div class="nmcard" id="bescard">
+                                    <div class="icircle gridb"><i class="fas fa-th"></i></div>
+                                    <h5>Bespoke Software</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Tailored software solutions to improve business productivity and
+                                        online
+                                        profits.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/digital-marketing">
+                                <div class="nmcard" id="digcard">
+                                    <div class="icircle sigb"><i class="fas fa-signal"></i></div>
+                                    <h5>Digital Marketing</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Driving brand awareness and ROI through creative digital marketing
+                                        campaigns.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/cyber-security">
+                                <div class="nmcard" id="cybercard">
+                                    <div class="icircle shieldb"><i class="fas fa-shield-alt"></i></div>
+                                    <h5>Cyber Security</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Ensuring your online business stays secure 24/7, 365 days of the
+                                        year.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+
+                        </div>
+
+                        <div id="cardset3">
+
+                            <a href="https://support.netmatters.com/telecoms-services">
+                                <div class="nmcard" id="telcard1">
+                                    <div class="icircle phoneb"><i class="fas fa-phone-volume"></i></div>
+                                    <h5>Telecoms Services</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Stay connected with bespoke telecoms solutions when you need it
+                                        most.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/bespoke-software">
+                                <div class="nmcard" id="bescard1">
+                                    <div class="icircle gridb"><i class="fas fa-th"></i></div>
+                                    <h5>Bespoke Software</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Tailored software solutions to improve business productivity and
+                                        online
+                                        profits.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div id="cardset4">
+                            <a href="https://support.netmatters.com/digital-marketing">
+                                <div class="nmcard" id="digcard1">
+                                    <div class="icircle sigb"><i class="fas fa-signal"></i></div>
+                                    <h5>Digital Marketing</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Driving brand awareness and ROI through creative digital marketing
+                                        campaigns.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+
+                            <a href="https://support.netmatters.com/cyber-security">
+                                <div class="nmcard" id="cybercard1">
+                                    <div class="icircle shieldb"><i class="fas fa-shield-alt"></i></div>
+                                    <h5>Cyber Security</h5>
+                                    <h2>&horbar;</h2>
+                                    <p class="cardtext">Ensuring your online business stays secure 24/7, 365 days of the
+                                        year.</p>
+                                    <p class="read-more">READ MORE</p>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                    </section>
+                    <div class="maintext-bg">
+                        <div class="nmcontainer">
+                            <div class="maintext">
+                                <h2>NETMATTERS</h2>
+                                <p class="bold">Netmatters Ltd is a leading web design, IT support and digital marketing
+                                    agency
+                                    based in Wymondham, Norfolk.</p>
+                                <p>Founded in 2008, we work with businesses from a variety of industries to gain new
+                                    prospects,
+                                    nurture existing leads and further grow their sales.</p>
+                                <p>We provide cost effective, reliable solutions to a range of services; from bespoke
+                                    cloud-based
+                                    management systems, workflow and IT solutions through to creative website development
+                                    and
+                                    integrated digital campaigning.</p>
+                            </div>
+                            <a href="/our-culture" id="culture">OUR CULTURE <i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>                
+<?php echo file_get_contents("html/footer.html"); ?>
